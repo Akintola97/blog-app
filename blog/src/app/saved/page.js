3d7989ed7afe1e88857d2a5e5a3ec16ce8c59794
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2025-04-05 22:30:57 -0500

    protected-route

--- blog/src/app/saved/page.js
+++ blog/src/app/saved/page.js
@@ -1,14 +1,18 @@
 import SavedBlogs from "@/components/SavedBlogs";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Saved() {
   const { isAuthenticated } = getKindeServerSession();
   const isUserAuthenticated = await isAuthenticated();
 
+  if(!isUserAuthenticated){
+    redirect('/');
+  }
 
-  return (
+  // return (
     <>
       <SavedBlogs isUserAuthenticated={isUserAuthenticated} />
     </>
-  );
+  // );
 }
